e8dab8ea038d053cf918274605cd35f6cd332c4f
kyle <jjyu9312@gmail.com>
2024-11-14 11:22:03 +0900

    entity 임시 생성 - user

---
+++ src/main/kotlin/com/kkw/petwalker/user/User.kt
@@ -0,0 +1,13 @@
+package com.kkw.petwalker.user
+
+data class User(
+    val id: Long,
+    val name: String,
+    val email: String,
+    val password: String,
+    val role: String,
+    val enabled: Boolean,
+    val accountNonExpired: Boolean,
+    val accountNonLocked: Boolean,
+    val credentialsNonExpired: Boolean,
+)
