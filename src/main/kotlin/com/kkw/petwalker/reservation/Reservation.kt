284431dc345d0b2100850cf1c24b7867788ed712
kyle <jjyu9312@gmail.com>
2024-11-18 15:39:35 +0900

    reservation class 생성

---
+++ src/main/kotlin/com/kkw/petwalker/reservation/Reservation.kt
@@ -0,0 +1,11 @@
+package com.kkw.petwalker.reservation
+
+data class Reservation(
+    val id: Long,
+    val pet: String,
+    val walker: String,
+    val date: String,
+    val time: String,
+    val duration: Int,
+    val status: String,
+)
