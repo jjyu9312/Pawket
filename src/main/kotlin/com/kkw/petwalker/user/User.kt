ab0a3dd7258c7b5e2d08fc1f8e986cc365082cc7
kyle <jjyu9312@gmail.com>
2024-11-19 09:52:59 +0900

    User entity 컬럼 정의

--- src/main/kotlin/com/kkw/petwalker/user/User.kt
+++ src/main/kotlin/com/kkw/petwalker/user/User.kt
@@ -1,13 +1,42 @@
 package com.kkw.petwalker.user
 
+import jakarta.persistence.*
+import java.time.LocalDate
+import java.util.UUID
+import java.util.regex.Pattern
+
+@Entity
 data class User(
-    val id: Long,
+
+    @Id
+    @Column(nullable = false, columnDefinition = "CHAR(36)")
+    val id: String = UUID.randomUUID().toString(),
+
+    @Column(nullable = false, length = 20)
     val name: String,
+
+    @Column(nullable = false)
+    val birth: LocalDate,
+
+    @Column(nullable = false, length = 50)
     val email: String,
-    val password: String,
-    val role: String,
-    val enabled: Boolean,
-    val accountNonExpired: Boolean,
-    val accountNonLocked: Boolean,
-    val credentialsNonExpired: Boolean,
-)
+
+    @Column(nullable = false)
+    val addressBasic: String,
+
+    @Column(nullable = false)
+    val addressLat: Float,
+
+    @Column(nullable = false)
+    val addressLng: Float,
+
+    @Column
+    val addressDetail: String,
+
+) {
+    fun isValidEmail(): Boolean {
+        val emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$"
+        val pattern = Pattern.compile(emailRegex)
+        return pattern.matcher(email).matches()
+    }
+}
