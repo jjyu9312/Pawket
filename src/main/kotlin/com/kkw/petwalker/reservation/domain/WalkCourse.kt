56886ec004fe919bc2166b5be850ab32122336de
kyle <jjyu9312@gmail.com>
2024-11-22 11:17:06 +0900

    Walk Course 엔티티 추가

--- src/main/kotlin/com/kkw/petwalker/reservation/domain/WalkCourse.kt
+++ src/main/kotlin/com/kkw/petwalker/reservation/domain/WalkCourse.kt
@@ -1,11 +1,26 @@
 package com.kkw.petwalker.reservation.domain
 
+import com.kkw.petwalker.common.domain.BaseEntity
+import jakarta.persistence.*
+import java.util.*
+
+@Entity
 data class WalkCourse (
-    val id: Long,
-    val pet: String,
-    val walker: String,
-    val date: String,
-    val time: String,
-    val duration: Int,
-    val status: String,
-)
+    @Id
+    @Column(nullable = false, columnDefinition = "CHAR(36)")
+    val id: String = UUID.randomUUID().toString(),
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reservation_id", columnDefinition = "CHAR(36)")
+    val reservation: Reservation,
+
+    @Column(nullable = false)
+    val name: String,
+
+    @Column(nullable = false)
+    val walkCourseLatList: String,
+
+    @Column(nullable = false)
+    val walkCourseLngList: String,
+
+): BaseEntity()
