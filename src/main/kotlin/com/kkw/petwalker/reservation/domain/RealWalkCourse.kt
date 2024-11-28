f8c48523a701fb4b823256036826d084c111da2c
kyle <jjyu9312@gmail.com>
2024-11-28 09:04:26 +0900

    RealWalkCourse 추가

---
+++ src/main/kotlin/com/kkw/petwalker/reservation/domain/RealWalkCourse.kt
@@ -0,0 +1,26 @@
+package com.kkw.petwalker.reservation.domain
+
+import com.kkw.petwalker.common.domain.BaseEntity
+import jakarta.persistence.*
+import java.util.*
+
+@Entity
+data class RealWalkCourse (
+    @Id
+    @Column(nullable = false, columnDefinition = "CHAR(36)")
+    val id: String = UUID.randomUUID().toString(),
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reservation_id", columnDefinition = "CHAR(36)")
+    val reservation: Reservation,
+
+    @Column(nullable = false)
+    val name: String,
+
+    @Column(nullable = false)
+    val realWalkCourseLatList: String,
+
+    @Column(nullable = false)
+    val realWalkCourseLngList: String,
+
+    ): BaseEntity()
