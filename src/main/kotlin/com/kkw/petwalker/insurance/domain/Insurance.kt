f3fcefec36ff26bec90febd5b279a2eb3ddddf71
kyle <jjyu9312@gmail.com>
2024-11-29 15:29:53 +0900

    insurance entity 추가

---
+++ src/main/kotlin/com/kkw/petwalker/insurance/domain/Insurance.kt
@@ -0,0 +1,20 @@
+package com.kkw.petwalker.insurance.domain
+
+import com.kkw.petwalker.common.domain.BaseEntity
+import jakarta.persistence.*
+
+@Entity
+data class Insurance(
+    @Id
+    @Column(nullable = false, columnDefinition = "CHAR(36)")
+    val id: String,
+
+    @Column(nullable = false)
+    val name: String,
+
+    @Column(nullable = false)
+    val content: String,
+
+    @Column(nullable = false)
+    val isRequired: Boolean = true,
+): BaseEntity()
