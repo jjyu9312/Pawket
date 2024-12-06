05116512c9193ed7434ffebf897203215782b075
kyle <jjyu9312@gmail.com>
2024-12-06 13:06:23 +0900

    createUser test

--- src/main/kotlin/com/kkw/petwalker/user/controller/UserController.kt
+++ src/main/kotlin/com/kkw/petwalker/user/controller/UserController.kt
@@ -1,7 +1,12 @@
 package com.kkw.petwalker.user.controller
 
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class UserController {
+    @PostMapping
+    fun createUser() {
+        println("User created")
+    }
 }
