a67a34b3409435fdc822b5db96ce567bc3ba1d00
kyle <jjyu9312@gmail.com>
2024-12-19 14:31:15 +0900

    string으로 Enum 찾을 있게 동반 객체로 메서드 추가

--- src/main/kotlin/com/kkw/petwalker/dog/domain/Sex.kt
+++ src/main/kotlin/com/kkw/petwalker/dog/domain/Sex.kt
@@ -1,6 +1,12 @@
 package com.kkw.petwalker.dog.domain
 
-enum class Sex {
-    MALE,
-    FEMALE,
+enum class Sex(val stringValue: String) {
+    MALE("male"),
+    FEMALE("female");
+
+    companion object {
+        fun fromString(value: String): Sex? {
+            return values().find { it.stringValue.equals(value, ignoreCase = true) }
+        }
+    }
 }
