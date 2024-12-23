a161656fad5a6965b05eff09fa3a8e55fbdc90fe
kyle <jjyu9312@gmail.com>
2024-12-23 10:23:41 +0900

    response code 추가

--- src/main/kotlin/com/kkw/petwalker/common/response/ResponseCode.kt
+++ src/main/kotlin/com/kkw/petwalker/common/response/ResponseCode.kt
@@ -1,11 +1,14 @@
 package com.kkw.petwalker.common.response
 
 enum class ResponseCode(val code: Int, val defaultMessage: String) {
-    SUCCESS(20000, "Request successful"),
-    BAD_REQUEST(40000, "Invalid request"),
-    NOT_FOUND(40400, "Resource not found"),
-    INTERNAL_SERVER_ERROR(50000, "Internal server error"),
-    WALKER_CREATION_FAILED(50001, "Failed to create walker");
+    SUCCESS(20000, "요청이 성공했습니다"),
+    BAD_REQUEST(40000, "잘못된 요청입니다"),
+    USER_CREATION_FAILED(40001, "user 생성에 실패했습니다"),
+    OWNER_CREATION_FAILED(40002, "owner 생성에 실패했습니다"),
+    WALKER_CREATION_FAILED(40003, "walker 생성에 실패했습니다"),
+    NOT_FOUND(40400, "리소스를 찾을 수 없습니다"),
+    INTERNAL_SERVER_ERROR(50000, "서버 내부 오류가 발생했습니다"),
+    ;
 
     fun withCustomMessage(customMessage: String?): String {
         return if (customMessage.isNullOrBlank()) defaultMessage else "$defaultMessage: $customMessage"
