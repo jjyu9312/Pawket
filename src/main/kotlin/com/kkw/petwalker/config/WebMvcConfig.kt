19248fad0866f3ec4e85f8899ce599e299af4d8e
kyle <jjyu9312@gmail.com>
2025-01-10 13:43:12 +0900

    WebMvcConfig 생성

---
+++ src/main/kotlin/com/kkw/petwalker/config/WebMvcConfig.kt
@@ -0,0 +1,16 @@
+package com.kkw.petwalker.config
+
+import com.kkw.petwalker.common.interceptor.LoggingInterceptor
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebMvcConfig(
+    private val loggingInterceptor: LoggingInterceptor
+) : WebMvcConfigurer {
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(loggingInterceptor)
+            .addPathPatterns("/**") // 모든 경로에 적용
+    }
+}
