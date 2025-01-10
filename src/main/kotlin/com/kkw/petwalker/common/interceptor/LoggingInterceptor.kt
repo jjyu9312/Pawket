34b1cc8d55b99cc32e97030355f6fd16bbec36bd
kyle <jjyu9312@gmail.com>
2025-01-10 13:42:54 +0900

    LoggingInterceptor 생성

---
+++ src/main/kotlin/com/kkw/petwalker/common/interceptor/LoggingInterceptor.kt
@@ -0,0 +1,30 @@
+package com.kkw.petwalker.common.interceptor
+
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
+
+@Component
+class LoggingInterceptor : HandlerInterceptor {
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
+    override fun preHandle(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        handler: Any
+    ): Boolean {
+        logger.info("Request URI: ${request.requestURI}, Method: ${request.method}")
+        return true
+    }
+
+    override fun afterCompletion(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        handler: Any,
+        ex: Exception?
+    ) {
+        logger.info("Response Status: ${response.status}")
+    }
+}
