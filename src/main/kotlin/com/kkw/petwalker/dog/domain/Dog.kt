0578c25dfbce3e8fa27eab0f3e7aac162834505a
jjyu9312 <jjyu9312@gmail.com>
2025-01-24 14:36:16 +0900

    컬럼 추가 (강아지 등록번호, 메인이미지, 강아지 간단 소개 정보)

--- src/main/kotlin/com/kkw/petwalker/dog/domain/Dog.kt
+++ src/main/kotlin/com/kkw/petwalker/dog/domain/Dog.kt
@@ -1,5 +1,7 @@
 package com.kkw.petwalker.dog.domain
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import com.kkw.petwalker.common.domain.BaseEntity
 import com.kkw.petwalker.user.domain.Owner
 import jakarta.persistence.*
@@ -15,6 +17,9 @@ data class Dog(
     @JoinColumn(name = "owner_id", columnDefinition = "CHAR(36)")
     val owner: Owner,
 
+    @Column(nullable = false, length = 50, unique = true)
+    val registrationNum: String,
+
     @Column(nullable = false, length = 50)
     val name: String,
 
@@ -22,8 +27,11 @@ data class Dog(
     @Enumerated(EnumType.STRING)
     val type: DogType,
 
+    @Column(nullable = false, unique = true)
+    val mainImageUrl: String,
+
     @Column(nullable = false) // ,로 연결
-    val imageUrl: String,
+    val imageUrls: String,
 
     @Column(nullable = false)
     val age: Int,
@@ -38,25 +46,57 @@ data class Dog(
     @Column(nullable = false)
     val isNeutered: Boolean = false,
 
-): BaseEntity() {
+    @Column(nullable = false, columnDefinition = "TEXT") // JSON 데이터 저장
+    val dogDetail: String,
+
+    ) : BaseEntity() {
+    companion object {
+        private val objectMapper = jacksonObjectMapper()
+
+        // Dog 상세 정보를 JSON으로 변환
+        fun createDogDetailJson(
+            dogDescription: String,
+            foodBrand: String,
+            foodName: String,
+            foodType: String
+        ): String {
+            val detailMap = mapOf(
+                "dog_description" to dogDescription,
+                "food_brand" to foodBrand,
+                "food_name" to foodName,
+                "food_type" to foodType
+            )
+            return objectMapper.writeValueAsString(detailMap)
+        }
+    }
+
     constructor(
         owner: Owner,
+        registrationNum: String,
         name: String,
         type: DogType,
-        imageUrl: String,
+        mainImageUrl: String,
+        imageUrls: String,
         age: Int,
         weight: Int,
         sex: Sex,
         isNeutered: Boolean,
+        dogDescription: String,
+        foodBrand: String,
+        foodName: String,
+        foodType: String,
     ) : this(
         id = UUID.randomUUID().toString(),
         owner = owner,
-        type = type,
+        registrationNum = registrationNum,
         name = name,
-        imageUrl = imageUrl,
+        type = type,
+        mainImageUrl = mainImageUrl,
+        imageUrls = imageUrls,
         age = age,
         weight = weight,
         sex = sex,
         isNeutered = isNeutered,
+        dogDetail = createDogDetailJson(dogDescription, foodBrand, foodName, foodType) // JSON 변환 후 저장
     )
 }
