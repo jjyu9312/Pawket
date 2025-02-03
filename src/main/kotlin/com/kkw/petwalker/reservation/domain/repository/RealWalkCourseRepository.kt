ba7aba8c75dbea9afce570ab5887c6a033604240
kyle <jjyu9312@gmail.com>
2025-02-03 20:44:37 +0900

    불필요한 파일 삭제

--- src/main/kotlin/com/kkw/petwalker/reservation/domain/repository/RealWalkCourseRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.kkw.petwalker.reservation.domain.repository
-
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface RealWalkCourseRepository: JpaRepository<RealWalkCourse, String> {
-}
