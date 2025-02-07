026daa4605a4d1a1a8e6c7e6592c616bfcc2805c
jjyu9312 <jjyu9312@gmail.com>
2025-02-07 13:36:53 +0900

    walker, owner 개념이 사라져 삭제

--- src/main/kotlin/com/kkw/petwalker/user/domain/repository/WalkerRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.kkw.petwalker.user.domain.repository
-
-import com.kkw.petwalker.user.domain.Walker
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface WalkerRepository: JpaRepository<Walker, String> {
-}
