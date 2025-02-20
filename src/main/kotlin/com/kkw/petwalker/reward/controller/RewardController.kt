96d6cf7107b73d4c9c3530a675319053367fe554
jjyu9312 <jjyu9312@gmail.com>
2025-02-20 15:12:38 +0900

    RewardController 생성

---
+++ src/main/kotlin/com/kkw/petwalker/reward/controller/RewardController.kt
@@ -0,0 +1,9 @@
+package com.kkw.petwalker.reward.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/reward")
+class RewardController {
+}
