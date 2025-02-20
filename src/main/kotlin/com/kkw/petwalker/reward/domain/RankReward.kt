c351db6f0c2f6cefccc0890202f207b52094bb3a
jjyu9312 <jjyu9312@gmail.com>
2025-02-20 15:12:53 +0900

    RankReward 생성

---
+++ src/main/kotlin/com/kkw/petwalker/reward/domain/RankReward.kt
@@ -0,0 +1,27 @@
+package com.kkw.petwalker.reward.domain
+
+import jakarta.persistence.Column
+import jakarta.persistence.DiscriminatorValue
+import jakarta.persistence.Entity
+import jakarta.persistence.PrimaryKeyJoinColumn
+
+@Entity
+@DiscriminatorValue("RANK")
+@PrimaryKeyJoinColumn(name = "reward_id")
+class RankReward(
+    @Column(nullable = false)
+    val rank: Int
+) : Reward(
+    coin = calculateCoin(rank)
+) {
+    companion object {
+        fun calculateCoin(rank: Int): Int {
+            return when (rank) {
+                1 -> 500
+                2 -> 300
+                3 -> 100
+                else -> 0
+            }
+        }
+    }
+}
