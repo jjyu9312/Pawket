3aa322df14e85f9c9e926f9a5df1b5c068dd7032
kyle <jjyu9312@gmail.com>
2025-02-22 11:49:28 +0900

    광고 정보 엔티티 추가

---
+++ src/main/kotlin/com/kkw/petwalker/ads/Ads.kt
@@ -0,0 +1,39 @@
+package com.kkw.petwalker.ads
+
+import com.kkw.petwalker.common.domain.BaseEntity
+import jakarta.persistence.*
+import java.util.*
+
+@Entity
+data class Ads(
+    @Id
+    @Column(nullable = false, columnDefinition = "CHAR(36)")
+    val id: String = UUID.randomUUID().toString(),
+
+    @JoinColumn(name = "company_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    val company: Company,
+
+    @Column(nullable = false)
+    val title: String,
+
+    @Column(nullable = true)
+    val content: String? = null,
+
+    @Column(nullable = true)
+    val reward: Int = 0,
+
+    ) : BaseEntity() {
+    constructor(
+        company: Company,
+        title: String,
+        content: String?,
+        reward: Int,
+    ) : this(
+        id = UUID.randomUUID().toString(),
+        company = company,
+        title = title,
+        content = content,
+        reward = reward,
+    )
+}
