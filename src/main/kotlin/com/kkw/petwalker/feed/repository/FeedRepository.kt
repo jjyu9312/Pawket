ca5417ef5322c91408282158df519816e3502984
jjyu9312 <jjyu9312@gmail.com>
2025-02-26 13:20:58 +0900

    kotlin google convention 적용

--- src/main/kotlin/com/kkw/petwalker/feed/repository/FeedRepository.kt
+++ src/main/kotlin/com/kkw/petwalker/feed/repository/FeedRepository.kt
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface FeedRepository: JpaRepository<Feed, String> {
+interface FeedRepository : JpaRepository<Feed, String> {
 }
--- src/main/kotlin/com/kkw/petwalker/pet/domain/repository/PetRepository.kt
+++ src/main/kotlin/com/kkw/petwalker/pet/domain/repository/PetRepository.kt
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface PetRepository: JpaRepository<Pet, String> {
+interface PetRepository : JpaRepository<Pet, String> {
 }
