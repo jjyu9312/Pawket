72ea4263405d7d8573a96f8469e3c79730d8fb36
jjyu9312 <jjyu9312@gmail.com>
2025-03-14 13:40:47 +0900

    약관 동의 관련 메서드 추가

--- src/main/kotlin/com/kkw/petwalker/terms/service/TermsService.kt
+++ src/main/kotlin/com/kkw/petwalker/terms/service/TermsService.kt
@@ -1,7 +1,74 @@
 package com.kkw.petwalker.terms.service
 
+import com.kkw.petwalker.common.response.ApiResponseFactory
+import com.kkw.petwalker.common.response.ResponseCode
+import com.kkw.petwalker.terms.domain.Terms
+import com.kkw.petwalker.terms.domain.UserTermsMapping
+import com.kkw.petwalker.terms.domain.repository.TermsRepository
+import com.kkw.petwalker.terms.domain.repository.UserTermsMappingRepository
+import com.kkw.petwalker.terms.model.req.TermsCreateReq
+import com.kkw.petwalker.terms.model.res.TermsListRes
+import com.kkw.petwalker.user.domain.repository.UserRepository
+import org.apache.coyote.BadRequestException
 import org.springframework.stereotype.Service
 
 @Service
-class TermsService {
+class TermsService(
+    private val termsRepository: TermsRepository,
+    private val userRepository: UserRepository,
+    private val userTermsMappingRepository: UserTermsMappingRepository,
+) {
+    fun createTerms(termsCreateReq: TermsCreateReq): String {
+        val terms = Terms(
+            title = termsCreateReq.title,
+            content = termsCreateReq.content,
+            isRequired = termsCreateReq.isRequired,
+        )
+
+        termsRepository.save(terms)
+
+        return terms.id
+    }
+
+    fun getTerms(): List<TermsListRes> {
+        val termsList = termsRepository.findAll()
+
+        if (termsList.isEmpty()) {
+            throw BadRequestException("약관이 존재하지 않습니다.")
+        }
+
+        return termsList.map {
+            TermsListRes(
+                id = it.id,
+                name = it.title,
+                url = it.content,
+                isRequired = it.isRequired,
+            )
+        }
+    }
+
+    fun agreeToTerms(userId: String, agreeTermsList: List<String>) {
+        val user = userRepository.findById(userId).orElseThrow {
+            throw BadRequestException("사용자가 존재하지 않습니다.")
+        }
+
+        val termsList = termsRepository.findAllById(agreeTermsList)
+
+        val alreadyAgreedTermsIdList = userTermsMappingRepository.findAllByUserIdAndTermsIdIn(userId, termsList.map { it.id })
+            .map { it.terms.id }
+
+        val duplicatedTerms = termsList.filter { alreadyAgreedTermsIdList.contains(it.id) }
+        if (duplicatedTerms.isNotEmpty()) {
+            throw BadRequestException("이미 동의한 약관은 다시 동의할 수 없습니다: $duplicatedTerms")
+        }
+
+        val newAgreeUserTermsMappingList = termsList.map { term ->
+            UserTermsMapping(
+                user = user,
+                terms = term,
+                isAgreed = true,
+            )
+        }
+        userTermsMappingRepository.saveAll(newAgreeUserTermsMappingList)
+    }
 }
