500a79b8231c14511940deb157f7fe724e73d381
jjyu9312 <jjyu9312@gmail.com>
2025-03-17 14:01:16 +0900

    함수 설명 주석 추가

--- src/main/kotlin/com/kkw/petwalker/terms/domain/repository/TermsRepositoryImpl.kt
+++ src/main/kotlin/com/kkw/petwalker/terms/domain/repository/TermsRepositoryImpl.kt
@@ -8,15 +8,18 @@ import org.springframework.stereotype.Repository
 
 @Repository
 class TermsRepositoryImpl(private val jpaQueryFactory: JPAQueryFactory) : TermsRepositoryCustom {
+    /*
+        * 유저가 동의하지 않은 필수 약관을 조회한다.
+     */
     override fun findRequiredTermsNotAgreedByUser(userId: String): List<Terms> {
         return jpaQueryFactory
             .selectFrom(terms)
             .leftJoin(userTermsMapping)
             .on(terms.id.eq(userTermsMapping.terms.id)
-                .and(userTermsMapping.user.id.eq(userId)))  // 유저와 약관 매핑된 경우 필터링
+                .and(userTermsMapping.user.id.eq(userId)))
             .where(
-                terms.isRequired.isTrue,  // 필수 약관만 선택
-                userTermsMapping.terms.id.isNull  // 유저가 동의하지 않은 약관만 조회
+                terms.isRequired.isTrue,
+                userTermsMapping.terms.id.isNull
             )
             .fetch()
     }
