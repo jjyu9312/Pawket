f0be7436d63c04e777c42e419878691a151b733c
jjyu9312 <jjyu9312@gmail.com>
2025-03-17 14:08:26 +0900

    Spot Controller 추가

---
+++ src/main/kotlin/com/kkw/petwalker/spot/controller/SpotController.kt
@@ -0,0 +1,9 @@
+package com.kkw.petwalker.spot.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/spot")
+class SpotController {
+}
