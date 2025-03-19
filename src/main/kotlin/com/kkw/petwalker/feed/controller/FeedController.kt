cd13f3e3a537eb87a2a4a8f8cda824fe44a7c8d5
jjyu9312 <jjyu9312@gmail.com>
2025-03-19 16:58:07 +0900

    Feed 관련 예상 API TODO 주석 추가

---
+++ src/main/kotlin/com/kkw/petwalker/feed/controller/FeedController.kt
@@ -0,0 +1,26 @@
+package com.kkw.petwalker.feed.controller
+
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class FeedController {
+    /*
+    TODO 피드 등록
+     */
+
+    /*
+    TODO 피드 정보 수정
+     */
+
+    /*
+    TODO 피드 정보 조회
+     */
+
+    /*
+    TODO userId로 피드 정보 조회
+     */
+
+    /*
+    TODO 인기 상위 5개 피드 목록 조회
+     */
+}
