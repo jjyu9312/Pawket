847fe05e646202b3633a9b48b586b447bd49959d
jjyu9312 <jjyu9312@gmail.com>
2025-03-19 14:55:14 +0900

    Spot 생성 메서드 개발

--- src/main/kotlin/com/kkw/petwalker/spot/service/SpotService.kt
+++ src/main/kotlin/com/kkw/petwalker/spot/service/SpotService.kt
@@ -1,7 +1,36 @@
 package com.kkw.petwalker.spot.service
 
+import com.kkw.petwalker.ads.repository.CompanyRepository
+import com.kkw.petwalker.spot.domain.ImportanceLevel
+import com.kkw.petwalker.spot.domain.Spot
+import com.kkw.petwalker.spot.domain.repository.SpotRepository
+import com.kkw.petwalker.spot.model.req.CreateSpotReq
+import org.apache.coyote.BadRequestException
 import org.springframework.stereotype.Service
 
 @Service
-class SpotService {
+class SpotService
+    (
+    private val spotRepository: SpotRepository,
+    private val companyRepository: CompanyRepository,
+) {
+    fun createSpot(companyId: String, req: CreateSpotReq): String? {
+        val company = companyRepository.findById(companyId)
+            .orElseThrow { BadRequestException("Company not found") }
+
+        val spot = Spot(
+            company = company,
+            name = req.name,
+            detail = req.detail,
+            importanceLevel = ImportanceLevel.fromString(req.importanceLevel)!!,
+            addressBasic = req.addressBasic,
+            addressLat = req.addressLat,
+            addressLng = req.addressLng,
+            addressDetail = req.addressDetail,
+        )
+
+        spotRepository.save(spot)
+
+        return spot.id
+    }
 }
