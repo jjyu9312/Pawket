aef54fd43e67fade98df0f8b5e62f34d4b15a080
jjyu9312 <jjyu9312@gmail.com>
2025-03-20 16:56:47 +0900

    Notice 관련 예상 API TODO 주석 추가

--- src/main/kotlin/com/kkw/petwalker/notice/controller/NoticeController.kt
+++ src/main/kotlin/com/kkw/petwalker/notice/controller/NoticeController.kt
@@ -6,4 +6,19 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/v1/notice")
 class NoticeController {
+    /*
+    TODO 공지사항 등록
+     */
+
+    /*
+    TODO 공지사항 정보 수정
+     */
+
+    /*
+    TODO 공지사항 정보 조회
+     */
+
+    /*
+    TODO 회원 유형으로 공지사항 정보 조회
+     */
 }
