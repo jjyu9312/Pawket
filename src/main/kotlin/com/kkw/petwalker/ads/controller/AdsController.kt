454b5ef56b89bd104831e06901d614629f508f62
kyle <jjyu9312@gmail.com>
2025-03-25 10:38:05 +0900

    controller 주석 추가

--- src/main/kotlin/com/kkw/petwalker/ads/controller/AdsController.kt
+++ src/main/kotlin/com/kkw/petwalker/ads/controller/AdsController.kt
@@ -6,4 +6,19 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/v1/ads")
 class AdsController {
+    /*
+    TODO Ads 생성
+     */
+
+    /*
+    TODO Ads 조회
+     */
+
+    /*
+    TODO Ads 수정
+     */
+
+    /*
+    TODO Ads 삭제
+     */
 }
