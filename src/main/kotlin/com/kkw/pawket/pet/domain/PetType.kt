1cc5287843e86aaf5e11cdc367781ee850327ac1
kyle <jjyu9312@gmail.com>
2025-03-27 14:45:57 +0900

    petType 한글에서 영어로 변경

--- src/main/kotlin/com/kkw/pawket/pet/domain/PetType.kt
+++ src/main/kotlin/com/kkw/pawket/pet/domain/PetType.kt
@@ -1,16 +1,16 @@
 package com.kkw.pawket.pet.domain
 
 enum class PetType(val stringValue: String) {
-    DOG("강아지"),
-    CAT("고양이"),
-    RABBIT("토끼"),
-    HAMSTER("햄스터"),
-    GUINEA_PIG("기니피그"),
-    BIRD("새"),
-    MEERKAT("미어캣"),
-    RACC00N("너구리"),
-    HEDGEHOG("고슴도치"),
-    ETC("기타"),
+    DOG("dog"),
+    CAT("cat"),
+    RABBIT("rabbit"),
+    HAMSTER("hamster"),
+    GUINEA_PIG("guinea_pig"),
+    BIRD("bird"),
+    MEERKAT("meerkat"),
+    RACC00N("raccoon"),
+    HEDGEHOG("hedghog"),
+    ETC("etc"),
     ;
 
     companion object {
