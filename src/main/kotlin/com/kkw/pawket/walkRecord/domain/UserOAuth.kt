e0e0a9974b9d991b372fc7c14c4f3f90f222c760
kyle <jjyu9312@gmail.com>
2025-03-27 14:46:24 +0900

    UserOAuth 엔티티 추가

---
+++ src/main/kotlin/com/kkw/pawket/walkRecord/domain/UserOAuth.kt
@@ -0,0 +1,30 @@
+package com.kkw.pawket.walkRecord.domain
+
+import com.kkw.pawket.common.domain.BaseEntity
+import com.kkw.pawket.user.domain.User
+import jakarta.persistence.*
+import java.util.*
+
+@Entity
+@Table(
+    name = "user_oauth", uniqueConstraints = [
+        UniqueConstraint(columnNames = ["provider", "providerUserId"])
+    ]
+)
+class UserOAuth(
+    @Id
+    @Column(nullable = false, columnDefinition = "CHAR(36)")
+    val id: String = UUID.randomUUID().toString(),
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    val user: User,
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    val provider: OAuthProvider,  // GOOGLE, KAKAO, APPLE
+
+    @Column(nullable = false)
+    val providerUserId: String,  // 구글: sub / 카카오: id / 애플: sub
+
+) : BaseEntity()
