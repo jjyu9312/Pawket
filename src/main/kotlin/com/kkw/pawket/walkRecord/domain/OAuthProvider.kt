0ccad16be46488424e9b8d03f64f4890c90a6c5c
kyle <jjyu9312@gmail.com>
2025-03-27 14:45:31 +0900

    OAuthProvider 추가

---
+++ src/main/kotlin/com/kkw/pawket/walkRecord/domain/OAuthProvider.kt
@@ -0,0 +1,14 @@
+package com.kkw.pawket.walkRecord.domain
+
+enum class OAuthProvider(val stringValue: String) {
+    GOOGLE("google"),
+    KAKAO("kakao"),
+    APPLE("apple"),
+    ;
+
+    companion object {
+        fun fromString(value: String): OAuthProvider? {
+            return OAuthProvider.entries.find { it.stringValue.equals(value, ignoreCase = true) }
+        }
+    }
+}
