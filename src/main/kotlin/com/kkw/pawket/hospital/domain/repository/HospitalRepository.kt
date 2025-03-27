128de743f3462ddb9fe5638dd7c54b2d56e65876
kyle <jjyu9312@gmail.com>
2025-03-27 14:45:13 +0900

    Hospital -> Partner로 네이밍 변경

--- src/main/kotlin/com/kkw/pawket/hospital/domain/repository/HospitalRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.kkw.pawket.hospital.domain.repository
-
-import com.kkw.pawket.hospital.domain.Hospital
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface HospitalRepository: JpaRepository<Hospital, String> {
-}
--- src/main/kotlin/com/kkw/pawket/hospital/service/HospitalService.kt
+++
@@ -1,7 +0,0 @@
-package com.kkw.pawket.hospital.service
-
-import org.springframework.stereotype.Service
-
-@Service
-class HospitalService {
-}
