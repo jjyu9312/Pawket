9ccad714242770c5dd964381e31871606d4d4dee
kyle <jjyu9312@gmail.com>
2025-03-27 14:46:03 +0900

    예약 엔티티 수정

--- src/main/kotlin/com/kkw/pawket/reservation/domain/Reservation.kt
+++ src/main/kotlin/com/kkw/pawket/reservation/domain/Reservation.kt
@@ -1,7 +1,7 @@
 package com.kkw.pawket.reservation.domain
 
 import com.kkw.pawket.common.domain.BaseEntity
-import com.kkw.pawket.hospital.domain.Hospital
+import com.kkw.pawket.partner.domain.Partner
 import com.kkw.pawket.user.domain.User
 import jakarta.persistence.*
 import java.util.*
@@ -18,24 +18,35 @@ data class Reservation(
 
     @JoinColumn(name = "hospital_id")
     @ManyToOne(fetch = FetchType.LAZY)
-    val hospital: Hospital,
+    val partner: Partner,
 
     @Column(nullable = false)
     val reservationDate: Date,
 
     @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    val status: ReservationStatus = ReservationStatus.REQUESTED,
+
+    @Column(nullable = false)
     val totalFee: Int = 0,
+
+    @Column(nullable = false)
+    val discountFee: Int = 0,
 ) : BaseEntity() {
     constructor(
         user: User,
-        hospital: Hospital,
+        partner: Partner,
         reservationDate: Date,
+        reservationStatus: ReservationStatus,
         totalFee: Int,
+        discountFee: Int,
     ) : this(
         id = UUID.randomUUID().toString(),
         user = user,
-        hospital = hospital,
+        partner = partner,
         reservationDate = reservationDate,
+        status = reservationStatus,
         totalFee = totalFee,
+        discountFee = discountFee,
     )
 }
