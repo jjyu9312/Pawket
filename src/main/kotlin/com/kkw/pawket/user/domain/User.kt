edc360477dba60f0e38b18f23d7accdf53d13b62
kyle <jjyu9312@gmail.com>
2025-03-28 14:04:47 +0900

    User 엔티티 생성 방법 변경

--- src/main/kotlin/com/kkw/pawket/user/domain/User.kt
+++ src/main/kotlin/com/kkw/pawket/user/domain/User.kt
@@ -12,14 +12,14 @@ data class User(
     val id: String,
 
     @Column(nullable = false, length = 20)
-    val name: String,
+    var name: String,
 
     @Column(nullable = false)
-    val birth: LocalDate,
+    var birth: LocalDate,
 
     @Column(nullable = false, length = 20)
     @Enumerated(value = EnumType.STRING)
-    val gender: Gender,
+    var gender: Gender,
 
     @Column(nullable = false, length = 50, unique = true)
     val email: String,
@@ -28,38 +28,50 @@ data class User(
     var totalCoin: Int = 0,
 
     @Column(nullable = false)
-    val addressBasic: String,
+    var addressBasic: String,
 
     @Column(nullable = false)
-    val addressLat: Double,
+    var addressLat: Double,
 
     @Column(nullable = false)
-    val addressLng: Double,
+    var addressLng: Double,
 
     @Column()
-    val addressDetail: String? = null,
+    var addressDetail: String? = null,
 
     ) : BaseEntity() {
-    constructor(
+
+    companion object {
+        fun createWithEmailOnly(email: String): User = User(
+            id = UUID.randomUUID().toString(),
+            name = email,
+            birth = LocalDate.now(),
+            gender = Gender.MALE,
+            email = email,
+            addressBasic = "주소 미입력",
+            addressLat = 0.0,
+            addressLng = 0.0,
+        )
+    }
+
+    fun update(
         name: String,
         birth: LocalDate,
         gender: Gender,
-        email: String,
         addressBasic: String,
         addressLat: Double,
         addressLng: Double,
-        addressDetail: String? = null,
-    ) : this(
-        id = UUID.randomUUID().toString(),
-        name = name,
-        birth = birth,
-        gender = gender,
-        email = email,
-        addressBasic = addressBasic,
-        addressLat = addressLat,
-        addressLng = addressLng,
-        addressDetail = addressDetail,
-    )
+        addressDetail: String? = null
+    ) {
+        this.name = name
+        this.birth = birth
+        this.gender = gender
+        this.addressBasic = addressBasic
+        this.addressLat = addressLat
+        this.addressLng = addressLng
+        this.addressDetail = addressDetail
+    }
+
 
     fun isValidEmail(): Boolean {
         val emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$"
