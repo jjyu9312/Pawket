4db635d87542d32fc6355a003beb825892577351
kyle <jjyu9312@gmail.com>
2025-03-31 10:17:36 +0900

    테이블 추가

---
+++ src/main/kotlin/com/kkw/pawket/partner/domain/PartnerVistHistory.kt
@@ -0,0 +1,38 @@
+package com.kkw.pawket.partner.domain
+
+import com.kkw.pawket.common.domain.BaseEntity
+import com.kkw.pawket.user.domain.User
+import jakarta.persistence.*
+import java.time.LocalDateTime
+import java.util.*
+
+@Entity
+data class PartnerVistHistory(
+    @Id
+    @Column(nullable = false, columnDefinition = "CHAR(36)")
+    val id: String = UUID.randomUUID().toString(),
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", columnDefinition = "CHAR(36)")
+    val user: User,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "partner_id", columnDefinition = "CHAR(36)")
+    val partner: Partner,
+
+    @Column(nullable = false)
+    val visitedAt: LocalDateTime = LocalDateTime.now()
+
+    ) : BaseEntity() {
+
+    constructor(
+        user: User,
+        partner: Partner,
+        visitedAt: LocalDateTime,
+    ) : this(
+        id = UUID.randomUUID().toString(),
+        user = user,
+        partner = partner,
+        visitedAt = visitedAt,
+    )
+}
