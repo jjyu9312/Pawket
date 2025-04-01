f26ca608b9a176b7c341d550c2aa6d9b3a082fc0
kyle <jjyu9312@gmail.com>
2025-04-01 16:40:47 +0900

    WalkRecord 생성 함수 추가

--- src/main/kotlin/com/kkw/pawket/walkRecord/controller/WalkRecordController.kt
+++ src/main/kotlin/com/kkw/pawket/walkRecord/controller/WalkRecordController.kt
@@ -1,13 +1,45 @@
 package com.kkw.pawket.walkRecord.controller
 
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import com.kkw.pawket.common.response.ApiResponse
+import com.kkw.pawket.common.response.ApiResponseFactory
+import com.kkw.pawket.common.response.ResponseCode
+import com.kkw.pawket.walkRecord.model.req.CreateWalkRecordReq
+import com.kkw.pawket.walkRecord.service.WalkRecordService
+import org.apache.coyote.BadRequestException
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/v1/walk-record")
-class WalkRecordController {
+class WalkRecordController (
+    private val walkRecordService: WalkRecordService,
+) {
     /*
     TODO 산책 기록 등록
+     */
+    @PostMapping("/user/{userId}")
+    fun createWalkRecord(
+        @PathVariable userId: String,
+        @RequestBody req: CreateWalkRecordReq,
+    ): ResponseEntity<ApiResponse<String>> {
+        return try {
+            val walkRecord = walkRecordService.createWalkRecord(userId, req)
+            ApiResponseFactory.success(walkRecord)
+        } catch (e: BadRequestException) {
+            ApiResponseFactory.error(
+                responseCode = ResponseCode.BAD_REQUEST,  // 400 응답 코드
+                customMessage = e.message
+            )
+        } catch (e: Exception) {
+            ApiResponseFactory.error(
+                responseCode = ResponseCode.INTERNAL_SERVER_ERROR,  // 500 응답 코드
+                customMessage = e.message
+            )
+        }
+    }
+
+    /*
+    TODO 산책 종료 시 위치 정보 등록
      */
 
     /*
