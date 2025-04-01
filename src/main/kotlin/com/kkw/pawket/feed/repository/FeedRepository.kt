18a206a82618340f46b23b225fede00d8227f2a6
kyle <jjyu9312@gmail.com>
2025-04-01 16:33:12 +0900

    Entity Feed domain 안으로 이동

--- src/main/kotlin/com/kkw/pawket/feed/repository/FeedRepository.kt
+++ src/main/kotlin/com/kkw/pawket/feed/repository/FeedRepository.kt
@@ -1,6 +1,6 @@
 package com.kkw.pawket.feed.repository
 
-import com.kkw.pawket.feed.Feed
+import com.kkw.pawket.feed.domain.Feed
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
