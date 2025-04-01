fa070896d2605bc5cf856870b127e2333240b316
kyle <jjyu9312@gmail.com>
2025-04-01 16:46:45 +0900

    findByIdAndIsDeleted 추가

--- src/main/kotlin/com/kkw/pawket/ads/repository/CompanyRepository.kt
+++ src/main/kotlin/com/kkw/pawket/ads/repository/CompanyRepository.kt
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository
 
 @Repository
 interface CompanyRepository: JpaRepository<Company, String> {
+    fun findByIdAndIsDeletedFalse(id: String): Company?
 }
