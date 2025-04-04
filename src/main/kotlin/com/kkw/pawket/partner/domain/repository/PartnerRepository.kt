5e0b6b95e499654f8fed5d5accc1a70963091899
kyle <jjyu9312@gmail.com>
2025-04-04 10:36:04 +0900

    PartnerRepository findAllByUserIdAndIsDeletedFalse 메서드 추가

--- src/main/kotlin/com/kkw/pawket/partner/domain/repository/PartnerRepository.kt
+++ src/main/kotlin/com/kkw/pawket/partner/domain/repository/PartnerRepository.kt
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Repository
 interface PartnerRepository: JpaRepository<Partner, String> {
     fun findByIdAndIsDeletedTrue(id: String): Partner?
     fun findAllByUserId(userId: String): List<Partner>
-
+    fun findAllByUserIdAndIsDeletedFalse(userId: String): List<Partner>
 }
