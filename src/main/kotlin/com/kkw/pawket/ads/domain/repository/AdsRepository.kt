6f2eb82d0bc36891c3724185fb992b9939896a88
kyle <jjyu9312@gmail.com>
2025-04-04 10:34:35 +0900

    AdsRepository findAllByCompanyIn 메서드 추가

--- src/main/kotlin/com/kkw/pawket/ads/domain/repository/AdsRepository.kt
+++ src/main/kotlin/com/kkw/pawket/ads/domain/repository/AdsRepository.kt
@@ -1,10 +1,11 @@
 package com.kkw.pawket.ads.domain.repository
 
 import com.kkw.pawket.ads.domain.Ads
+import com.kkw.pawket.ads.domain.Company
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface AdsRepository: JpaRepository<Ads, String> {
-    fun findAllByUserId(userId: String): List<Ads>
+    fun findAllByCompanyIn(companies: List<Company>): List<Ads>
 }
