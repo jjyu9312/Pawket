83fff389845f5d3e3ae6669699d8d5870a6c57c0
kyle <jjyu9312@gmail.com>
2025-04-08 12:40:35 +0900

    S3 Upload Service 생성

---
+++ src/main/kotlin/com/kkw/pawket/common/service/S3UploadService.kt
@@ -0,0 +1,98 @@
+package com.kkw.pawket.common.service
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+import org.springframework.web.multipart.MultipartFile
+import software.amazon.awssdk.core.sync.RequestBody
+import software.amazon.awssdk.services.s3.S3Client
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest
+import software.amazon.awssdk.services.s3.model.PutObjectRequest
+import java.util.UUID
+
+@Service
+class S3UploadService(
+    private val s3Client: S3Client
+) {
+    @Value("\${cloud.aws.s3.bucket.name}")
+    private lateinit var bucketName: String
+
+    /**
+     * 단일 파일 업로드
+     * @param file 업로드할 MultipartFile
+     * @param dirName S3 내 디렉토리 이름
+     * @return 업로드된 파일의 S3 URL
+     */
+    fun uploadSingleFile(file: MultipartFile?, dirName: String = "default"): String? {
+        return file?.let {
+            try {
+                val originalFilename = it.originalFilename ?: return null
+                val fileName = generateFileName(dirName, originalFilename)
+
+                val putObjectRequest = PutObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(fileName)
+                    .contentType(it.contentType)
+                    .build()
+
+                s3Client.putObject(
+                    putObjectRequest,
+                    RequestBody.fromInputStream(it.inputStream, it.size)
+                )
+
+                generateS3Url(fileName)
+            } catch (e: Exception) {
+                null
+            }
+        }
+    }
+
+    /**
+     * 다중 파일 업로드
+     * @param files 업로드할 MultipartFile 리스트
+     * @param dirName S3 내 디렉토리 이름
+     * @return 업로드된 파일들의 S3 URL 리스트
+     */
+    fun uploadMultipleFiles(files: List<MultipartFile>?, dirName: String = "default"): List<String> {
+        return files?.mapNotNull {
+            uploadSingleFile(it, dirName)
+        } ?: emptyList()
+    }
+
+    /**
+     * 파일명 생성 (중복 방지)
+     */
+    private fun generateFileName(dirName: String, originalFilename: String): String {
+        val uuid = UUID.randomUUID().toString()
+        return "$dirName/$uuid-$originalFilename"
+    }
+
+    /**
+     * S3 URL 생성
+     */
+    private fun generateS3Url(fileName: String): String {
+        return "https://$bucketName.s3.amazonaws.com/$fileName"
+    }
+
+    /**
+     * S3에서 파일 삭제
+     * @param fileUrl 삭제할 파일의 S3 URL
+     */
+    fun deleteFile(fileUrl: String?) {
+        fileUrl?.let {
+            val fileName = extractKeyFromUrl(it)
+            val deleteObjectRequest = DeleteObjectRequest.builder()
+                .bucket(bucketName)
+                .key(fileName)
+                .build()
+
+            s3Client.deleteObject(deleteObjectRequest)
+        }
+    }
+
+    /**
+     * S3 URL에서 파일 키 추출
+     */
+    private fun extractKeyFromUrl(fileUrl: String): String {
+        return fileUrl.substringAfter("$bucketName.s3.amazonaws.com/")
+    }
+}
