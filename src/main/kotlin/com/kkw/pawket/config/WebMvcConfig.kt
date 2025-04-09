dc5883e27246e7d52e0a529cb9415589e5a36ac0
kyle <jjyu9312@gmail.com>
2025-04-09 10:14:15 +0900

    AuthorizationInterceptor.kt가 Spring Security Config와 충돌이 발생해 삭제

--- src/main/kotlin/com/kkw/pawket/config/WebMvcConfig.kt
+++ src/main/kotlin/com/kkw/pawket/config/WebMvcConfig.kt
@@ -1,6 +1,5 @@
 package com.kkw.pawket.config
 
-import com.kkw.pawket.common.interceptor.AuthorizationInterceptor
 import com.kkw.pawket.common.interceptor.LoggingInterceptor
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
@@ -9,16 +8,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 @Configuration
 class WebMvcConfig(
     private val loggingInterceptor: LoggingInterceptor,
-    private val authorizationInterceptor: AuthorizationInterceptor,
 ) : WebMvcConfigurer {
     override fun addInterceptors(registry: InterceptorRegistry) {
         registry.addInterceptor(loggingInterceptor)
             .addPathPatterns("/**")
             .excludePathPatterns("/test/**")
             .excludePathPatterns("/common/**")
-
-        registry.addInterceptor(authorizationInterceptor)
-            .addPathPatterns("/**")  // 모든 경로에 적용
-            .excludePathPatterns("/test/**", "/common/**", "/auth/**")  // 인증이 필요 없는 경로 제외
     }
 }
