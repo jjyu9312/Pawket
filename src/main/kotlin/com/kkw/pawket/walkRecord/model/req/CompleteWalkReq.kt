ffecedd8e0e04f1732833388601fa09863c3fb02
kyle <jjyu9312@gmail.com>
2025-04-14 16:44:36 +0900

    산책 위도, 경도 swagger description 수정

--- src/main/kotlin/com/kkw/pawket/walkRecord/model/req/CompleteWalkReq.kt
+++ src/main/kotlin/com/kkw/pawket/walkRecord/model/req/CompleteWalkReq.kt
@@ -8,11 +8,11 @@ data class CompleteWalkReq(
     @Schema(description = "산책 거리", example = "1000")
     val distance: Int,
 
-    @Schema(description = "산책 경로 위도", example = "37.123456")
-    val coordinateLat: Double,
+    @Schema(description = "산책 종료 경로 위도", example = "37.123456")
+    val lat: Double,
 
-    @Schema(description = "산책 경로 경도", example = "127.123456")
-    val coordinateLng: Double,
+    @Schema(description = "산책 종료 경로 경도", example = "127.123456")
+    val lng: Double,
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     val finishedAt: LocalDateTime,
