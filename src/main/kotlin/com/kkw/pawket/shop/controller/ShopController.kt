bb0cbbe3e2f2217172781812ae94c4b311bbd259
kyle <jjyu9312@gmail.com>
2025-04-17 15:53:53 +0900

    Shop 관련 controller 추가

---
+++ src/main/kotlin/com/kkw/pawket/shop/controller/ShopController.kt
@@ -0,0 +1,26 @@
+package com.kkw.pawket.shop.controller
+
+import com.kkw.pawket.shop.service.ShopService
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api/v1/shop")
+class ShopController(
+    private val shopService: ShopService
+) {
+    /*
+    TODO Shop Company 생성
+     */
+
+    /*
+    TODO Shop Company 조회
+     */
+
+    /*
+    TODO Shop Company 수정
+     */
+
+    /*
+    TODO Shop Company 삭제
+     */
+}
