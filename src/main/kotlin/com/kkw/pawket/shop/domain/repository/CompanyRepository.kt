783b327214fb492730b6a84e4f2d14595e8404b8
kyle <jjyu9312@gmail.com>
2025-04-17 15:53:37 +0900

    Shop 관련 Repository 추가

---
+++ src/main/kotlin/com/kkw/pawket/shop/domain/repository/CompanyRepository.kt
@@ -0,0 +1,11 @@
+package com.kkw.pawket.ads.domain.repository
+
+import com.kkw.pawket.shop.domain.Shop
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface ShopRepository: JpaRepository<Shop, String> {
+    fun findByIdAndIsDeletedFalse(id: String): Shop?
+    fun findAllByUserId(userId: String): List<Shop>
+}
