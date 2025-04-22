65dae6b17a5d0ce645172951fbc219fa8a3c9f64
kyle <jjyu9312@gmail.com>
2025-04-22 16:48:57 +0900

    잘못된 경로 및 함수 이름 변경

--- src/main/kotlin/com/kkw/pawket/shop/service/ShopService.kt
+++ src/main/kotlin/com/kkw/pawket/shop/service/ShopService.kt
@@ -1,6 +1,7 @@
 package com.kkw.pawket.shop.service
 
-import com.kkw.pawket.ads.domain.repository.ShopRepository
+
+import com.kkw.pawket.shop.domain.repository.ShopRepository
 import com.kkw.pawket.user.domain.repository.UserRepository
 import org.springframework.stereotype.Service
 
