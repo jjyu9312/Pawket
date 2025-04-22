40fa024d1aae1b91a004be68eff392995488cc93
kyle <jjyu9312@gmail.com>
2025-04-22 16:44:23 +0900

    shopService에 repository 추가

--- src/main/kotlin/com/kkw/pawket/shop/service/ShopService.kt
+++ src/main/kotlin/com/kkw/pawket/shop/service/ShopService.kt
@@ -1,10 +1,12 @@
 package com.kkw.pawket.shop.service
 
+import com.kkw.pawket.ads.domain.repository.ShopRepository
 import com.kkw.pawket.user.domain.repository.UserRepository
 import org.springframework.stereotype.Service
 
 @Service
 class ShopService(
     private val userRepository: UserRepository,
+    private val shopRepository: ShopRepository,
 ) {
 }
