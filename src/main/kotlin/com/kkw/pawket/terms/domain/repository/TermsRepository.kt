04024ca02969261f415932d20543614d02e17956
kyle <jjyu9312@gmail.com>
2025-05-15 14:29:48 +0900

    findByIdAndIsDeletedFalse 메서드 추가

--- src/main/kotlin/com/kkw/pawket/terms/domain/repository/TermsRepository.kt
+++ src/main/kotlin/com/kkw/pawket/terms/domain/repository/TermsRepository.kt
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository
 @Repository
 interface TermsRepository : JpaRepository<Terms, String>, TermsRepositoryCustom {
     fun findAllByIsRequiredTrue(): List<Terms>
+
+    fun findByIdAndIsDeletedFalse(id: String): Terms?
 }
