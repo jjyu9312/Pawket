83617fb70e6178bbbd2e261e4b17c0f81ebad2e4
kyle <jjyu9312@gmail.com>
2025-05-23 15:02:04 +0900

    UserReward -> UserPoint

--- src/main/kotlin/com/kkw/pawket/point/domain/UserPointHistory.kt
+++ src/main/kotlin/com/kkw/pawket/point/domain/UserPointHistory.kt
@@ -1,24 +1,23 @@
-package com.kkw.pawket.reward.domain
+package com.kkw.pawket.point.domain
 
 import com.kkw.pawket.common.domain.BaseEntity
 import com.kkw.pawket.user.domain.User
 import jakarta.persistence.*
-import java.util.*
 
 @Entity
-data class RewardHistory(
+data class UserPointHistory(
     @EmbeddedId
-    val id: RewardHistoryId,
+    val id: PointHistoryId,
 
     @MapsId("userId")
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", columnDefinition = "CHAR(36)")
     val user: User,
 
-    @MapsId("rewardId")
+    @MapsId("pointId")
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "reward_id", columnDefinition = "CHAR(36)")
-    val reward: Reward,
+    @JoinColumn(name = "point_id", columnDefinition = "CHAR(36)")
+    val point: Point,
 
     @Column(nullable = false)
     val beforeCoin: Int,
---
+++ src/main/kotlin/com/kkw/pawket/point/domain/repository/UserPointHistoryRepository.kt
@@ -0,0 +1,11 @@
+package com.kkw.pawket.point.domain.repository
+
+import com.kkw.pawket.point.domain.UserPointHistory
+import com.kkw.pawket.point.domain.PointHistoryId
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface UserPointHistoryRepository: JpaRepository<UserPointHistory, PointHistoryId> {
+    fun findAllByUserId(userId: String): List<UserPointHistory>
+}
