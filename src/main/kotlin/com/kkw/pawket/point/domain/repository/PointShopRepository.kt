82d27ea40c75510467fe255e8f9653ee6535c3ec
kyle <jjyu9312@gmail.com>
2025-06-11 16:48:05 +0900

    PointShop 레포지토리 생성

---
+++ src/main/kotlin/com/kkw/pawket/point/domain/repository/PointShopRepository.kt
@@ -0,0 +1,9 @@
+package com.kkw.pawket.point.domain.repository
+
+import com.kkw.pawket.point.domain.PointShop
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface PointShopRepository : JpaRepository<PointShop, String> {
+}
