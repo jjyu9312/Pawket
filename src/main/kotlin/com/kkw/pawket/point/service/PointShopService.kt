b996aab0794ec2a9bb73ff61d62c8533518e9f84
kyle <jjyu9312@gmail.com>
2025-06-12 14:20:44 +0900

    PointShop 생성 함수 개발

--- src/main/kotlin/com/kkw/pawket/point/service/PointShopService.kt
+++ src/main/kotlin/com/kkw/pawket/point/service/PointShopService.kt
@@ -1,14 +1,33 @@
 package com.kkw.pawket.point.service
 
+import com.kkw.pawket.common.exception.BadRequestException
+import com.kkw.pawket.common.response.ResponseCode
+import com.kkw.pawket.point.domain.PointShop
+import com.kkw.pawket.point.domain.repository.PointShopItemRepository
+import com.kkw.pawket.point.domain.repository.PointShopRepository
 import com.kkw.pawket.point.model.req.CreatePointShopItemReq
 import com.kkw.pawket.point.model.req.CreatePointShopReq
 import com.kkw.pawket.point.model.res.ReadPointShopRes
+import com.kkw.pawket.user.domain.repository.UserRepository
 import org.springframework.stereotype.Service
 
 @Service
-class PointShopService {
+class PointShopService(
+    private val userRepository: UserRepository,
+    private val pointShopRepository: PointShopRepository,
+    private val pointShopItemRepository: PointShopItemRepository,
+) {
     fun createPointShop(userId: String, req: CreatePointShopReq): String {
-        TODO("Not yet implemented")
+        if (!userRepository.existsByIdAndIsDeletedFalse(userId))
+            throw BadRequestException(ResponseCode.USER_NOT_FOUND)
+
+        if (pointShopRepository.existsByNameAndIsDeletedFalse(req.name))
+            throw BadRequestException(ResponseCode.POINT_SHOP_NAME_DUPLICATED)
+
+        val pointShop = PointShop.create(req.name)
+        pointShopRepository.save(pointShop)
+
+        return pointShop.id
     }
 
     fun createPointShopItem(userId: String, req: CreatePointShopItemReq): String {
