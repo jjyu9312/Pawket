d4c5e39ccb7016ee4cc64418a94698e68e722dc4
kyle <jjyu9312@gmail.com>
2025-06-12 14:24:27 +0900

    포인트샵 제품 생성 함수 개발

--- src/main/kotlin/com/kkw/pawket/point/service/PointShopService.kt
+++ src/main/kotlin/com/kkw/pawket/point/service/PointShopService.kt
@@ -3,6 +3,7 @@ package com.kkw.pawket.point.service
 import com.kkw.pawket.common.exception.BadRequestException
 import com.kkw.pawket.common.response.ResponseCode
 import com.kkw.pawket.point.domain.PointShop
+import com.kkw.pawket.point.domain.PointShopItem
 import com.kkw.pawket.point.domain.repository.PointShopItemRepository
 import com.kkw.pawket.point.domain.repository.PointShopRepository
 import com.kkw.pawket.point.model.req.CreatePointShopItemReq
@@ -18,20 +19,38 @@ class PointShopService(
     private val pointShopItemRepository: PointShopItemRepository,
 ) {
     fun createPointShop(userId: String, req: CreatePointShopReq): String {
-        if (!userRepository.existsByIdAndIsDeletedFalse(userId))
+        if (userRepository.existsByIdAndIsDeletedFalse(userId))
             throw BadRequestException(ResponseCode.USER_NOT_FOUND)
 
         if (pointShopRepository.existsByNameAndIsDeletedFalse(req.name))
             throw BadRequestException(ResponseCode.POINT_SHOP_NAME_DUPLICATED)
 
-        val pointShop = PointShop.create(req.name)
+        val pointShop = PointShop.create(
+            name = req.name
+        )
         pointShopRepository.save(pointShop)
 
         return pointShop.id
     }
 
     fun createPointShopItem(userId: String, req: CreatePointShopItemReq): String {
-        TODO("Not yet implemented")
+        if (userRepository.existsByIdAndIsDeletedFalse(userId))
+            throw BadRequestException(ResponseCode.USER_NOT_FOUND)
+
+        if(!pointShopItemRepository.existsByNameAndIsDeletedFalse(req.pointShopId))
+            throw BadRequestException(ResponseCode.POINT_SHOP_ITEM_NAME_DUPLICATED)
+
+        val pointShop = pointShopRepository.findByIdAndIsDeletedFalse(req.pointShopId)
+            ?: throw BadRequestException(ResponseCode.POINT_SHOP_NOT_FOUND)
+
+        val pointShopItem = PointShopItem.create(
+            pointShop = pointShop,
+            name = req.name,
+            price = req.price,
+        )
+        pointShopItemRepository.save(pointShopItem)
+
+        return pointShopItem.id
     }
 
     fun getPointShopsByUser(userId: String): List<ReadPointShopRes>? {
