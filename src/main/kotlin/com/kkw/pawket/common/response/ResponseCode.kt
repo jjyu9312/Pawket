97b0b41b713334d2fcd5d9972b8b9a1c2c9792f3
kyle <jjyu9312@gmail.com>
2025-06-12 14:21:02 +0900

    POINT_SHOP_NAME_DUPLICATED 에러 코드 추가

--- src/main/kotlin/com/kkw/pawket/common/response/ResponseCode.kt
+++ src/main/kotlin/com/kkw/pawket/common/response/ResponseCode.kt
@@ -15,6 +15,7 @@ enum class ResponseCode(val code: Int, val defaultMessage: String) {
     EMAIL_NOT_FOUND(40013, "이메일을 찾을 수 없습니다"),
     TERMS_NOT_FOUND(40014, "약관을 찾을 수 없습니다"),
     NOTICE_NOT_FOUND(40015, "공지사항을 찾을 수 없습니다"),
+    POINT_SHOP_NAME_DUPLICATED(40016, "포인트샵 이름이 중복되었습니다"),
     // NOTFOUND
 
 
