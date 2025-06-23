2796ed212e868a261897b5dc122e9a69fb27bd69
kyle <jjyu9312@gmail.com>
2025-06-23 13:06:45 +0900

    point1 -> point 수정

--- src/main/kotlin/com/kkw/pawket/point/domain/repository/UserPointRepositoryImpl.kt
+++ src/main/kotlin/com/kkw/pawket/point/domain/repository/UserPointRepositoryImpl.kt
@@ -2,7 +2,7 @@ package com.kkw.pawket.point.domain.repository
 
 import com.kkw.pawket.point.domain.PointType
 import com.kkw.pawket.point.domain.QUserPointHistory.userPointHistory
-import com.kkw.pawket.point.domain.QPoint.point1
+import com.kkw.pawket.point.domain.QPoint.point
 import com.kkw.pawket.user.domain.User
 import com.querydsl.jpa.impl.JPAQueryFactory
 import org.springframework.stereotype.Repository
@@ -13,10 +13,10 @@ class UserPointRepositoryImpl(private val jpaQueryFactory: JPAQueryFactory) : Us
         return jpaQueryFactory
             .select(userPointHistory.count())
             .from(userPointHistory)
-            .on(userPointHistory.point.id.eq(point1.id))
+            .on(userPointHistory.point.id.eq(point.id))
             .where(
                 userPointHistory.user.eq(user)
-                    .and(point1.type.eq(type))
+                    .and(point.type.eq(type))
             )
             .fetchOne() ?: 0L
     }
