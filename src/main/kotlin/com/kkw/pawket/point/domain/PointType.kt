ca62228fe85cd0ab606ac620b57ed067302bb37d
kyle <jjyu9312@gmail.com>
2025-07-11 13:25:12 +0900

    PointType enum 추가

--- src/main/kotlin/com/kkw/pawket/point/domain/PointType.kt
+++ src/main/kotlin/com/kkw/pawket/point/domain/PointType.kt
@@ -4,6 +4,8 @@ enum class PointType(val stringValue: String) {
     WALK("walk"),
     AD("ad"),
     FEED("feed"),
+    POINT_SHOP_ITEM("point_shop_item"),
+    COUPON("coupon"),
     ETC("etc"),
     ;
 
