f78db82999bb1b85abe702576e2bd1aebe7e6b15
kyle <jjyu9312@gmail.com>
2025-07-28 15:43:00 +0900

    RESTful 방식에서는 복수형 선호

--- src/main/kotlin/com/kkw/pawket/post/controller/PostController.kt
+++ src/main/kotlin/com/kkw/pawket/post/controller/PostController.kt
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/v1/post")
+@RequestMapping("/api/v1/posts")
 class PostController {
     /*
     TODO 포스트 등록
