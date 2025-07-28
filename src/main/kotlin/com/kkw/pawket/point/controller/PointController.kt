cc1b330815d274f1a40e9f9c5431d9e8b227595f
kyle <jjyu9312@gmail.com>
2025-07-28 15:43:12 +0900

    RESTful 방식에서는 복수형 선호

--- src/main/kotlin/com/kkw/pawket/point/controller/PointController.kt
+++ src/main/kotlin/com/kkw/pawket/point/controller/PointController.kt
@@ -14,14 +14,13 @@ import org.apache.coyote.BadRequestException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/v1/point")
+@RequestMapping("/api/v1/points")
 class PointController(
     private val pointService: PointService,
     private val pointShopService: PointShopService,
