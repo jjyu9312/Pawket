ec0325ae746f8ad4df47708f1946ec75fae39de7
kyle <jjyu9312@gmail.com>
2025-07-29 10:27:32 +0900

    Shop 생성 Req Dto 추가

---
+++ src/main/kotlin/com/kkw/pawket/shop/model/req/CreateShopReq.kt
@@ -0,0 +1,20 @@
+package com.kkw.pawket.shop.model.req
+
+import io.swagger.v3.oas.annotations.media.Schema
+
+data class CreateShopReq(
+    @Schema(description = "Name of the shop", example = "coupang")
+    val name: String,
+
+    @Schema(description = "address of the shop", example = "서울시 강남구 테헤란로 123")
+    val addressBasic: String,
+
+    @Schema(description = "Latitude of the shop address", example = "37.123456")
+    val addressLat: Double,
+
+    @Schema(description = "Longitude of the shop address", example = "127.123456")
+    val addressLng: Double,
+
+    @Schema(description = "Detailed address of the shop", example = "4층")
+    val addressDetail: String,
+)
