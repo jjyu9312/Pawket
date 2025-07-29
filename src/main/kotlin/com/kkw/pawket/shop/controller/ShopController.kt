7a2be9e736317ab238cba0f7030d06c28bd04fac
kyle <jjyu9312@gmail.com>
2025-07-29 10:28:22 +0900

    Shop 생성, 수정, 삭제 API 추가

--- src/main/kotlin/com/kkw/pawket/shop/controller/ShopController.kt
+++ src/main/kotlin/com/kkw/pawket/shop/controller/ShopController.kt
@@ -1,6 +1,18 @@
 package com.kkw.pawket.shop.controller
 
+import com.kkw.pawket.common.response.ApiResponse
+import com.kkw.pawket.common.response.ApiResponseFactory
+import com.kkw.pawket.common.response.ResponseCode
+import com.kkw.pawket.partner.model.req.CreatePartnerReq
+import com.kkw.pawket.shop.model.req.CreateShopReq
+import com.kkw.pawket.shop.model.req.UpdateShopReq
 import com.kkw.pawket.shop.service.ShopService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import org.apache.coyote.BadRequestException
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -11,6 +23,32 @@ class ShopController(
     /*
     TODO Shop Company 생성
      */
+    @Operation(
+        summary = "Shop Company 생성",
+        description = "새로운 Shop Company를 생성합니다."
+    )
+    @PostMapping
+    fun createShop(
+        @AuthenticationPrincipal userId: String,
+        @RequestBody req: CreateShopReq
+    ): ResponseEntity<ApiResponse<String>> {
+        return try {
+            val shopId = shopService.createShop(userId, req)
+            ApiResponseFactory.success(shopId)
+        } catch (e: BadRequestException) {
+            ApiResponseFactory.error(
+                responseCode = ResponseCode.BAD_REQUEST,  // 400 응답 코드
+                httpStatus = HttpStatus.BAD_REQUEST,
+                customMessage = e.message
+            )
+        } catch (e: Exception) {
+            ApiResponseFactory.error(
+                responseCode = ResponseCode.INTERNAL_SERVER_ERROR,  // 500 응답 코드
+                httpStatus = HttpStatus.INTERNAL_SERVER_ERROR,
+                customMessage = e.message
+            )
+        }
+    }
 
     /*
     TODO Shop Company 조회
@@ -19,8 +57,53 @@ class ShopController(
     /*
     TODO Shop Company 수정
      */
+    @PutMapping("{shopId}")
+    fun updateShop(
+        @AuthenticationPrincipal userId: String,
+        @PathVariable shopId: String,
+        @RequestBody req: UpdateShopReq,
+    ): ResponseEntity<ApiResponse<String>> {
+        return try {
+            shopService.updateShop(userId, shopId, req)
+            ApiResponseFactory.success("Shop updated successfully")
+        } catch (e: BadRequestException) {
+            ApiResponseFactory.error(
+                responseCode = ResponseCode.BAD_REQUEST,
+                httpStatus = HttpStatus.BAD_REQUEST,
+                customMessage = e.message
+            )
+        } catch (e: Exception) {
+            ApiResponseFactory.error(
+                responseCode = ResponseCode.INTERNAL_SERVER_ERROR,
+                httpStatus = HttpStatus.INTERNAL_SERVER_ERROR,
+                customMessage = e.message
+            )
+        }
+    }
 
     /*
     TODO Shop Company 삭제
      */
+    @DeleteMapping("{shopId}")
+    fun deleteShop(
+        @AuthenticationPrincipal userId: String,
+        @PathVariable shopId: String
+    ): ResponseEntity<ApiResponse<String>> {
+        return try {
+            shopService.deleteShop(userId, shopId)
+            ApiResponseFactory.success("Shop deleted successfully")
+        } catch (e: BadRequestException) {
+            ApiResponseFactory.error(
+                responseCode = ResponseCode.BAD_REQUEST,
+                httpStatus = HttpStatus.BAD_REQUEST,
+                customMessage = e.message
+            )
+        } catch (e: Exception) {
+            ApiResponseFactory.error(
+                responseCode = ResponseCode.INTERNAL_SERVER_ERROR,
+                httpStatus = HttpStatus.INTERNAL_SERVER_ERROR,
+                customMessage = e.message
+            )
+        }
+    }
 }
