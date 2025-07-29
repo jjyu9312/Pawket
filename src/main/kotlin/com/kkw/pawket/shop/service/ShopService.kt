39ccdac65f387c5cdc53bbf54fcc07dc27a29f86
kyle <jjyu9312@gmail.com>
2025-07-29 10:28:16 +0900

    Shop 생성, 수정, 삭제 함수 추가

--- src/main/kotlin/com/kkw/pawket/shop/service/ShopService.kt
+++ src/main/kotlin/com/kkw/pawket/shop/service/ShopService.kt
@@ -1,7 +1,12 @@
 package com.kkw.pawket.shop.service
 
 
+import com.kkw.pawket.common.exception.BadRequestException
+import com.kkw.pawket.common.response.ResponseCode
+import com.kkw.pawket.shop.domain.Shop
 import com.kkw.pawket.shop.domain.repository.ShopRepository
+import com.kkw.pawket.shop.model.req.CreateShopReq
+import com.kkw.pawket.shop.model.req.UpdateShopReq
 import com.kkw.pawket.user.domain.repository.UserRepository
 import org.springframework.stereotype.Service
 
@@ -10,4 +15,47 @@ class ShopService(
     private val userRepository: UserRepository,
     private val shopRepository: ShopRepository,
 ) {
+    fun createShop(userId: String, req: CreateShopReq): String {
+        val user = userRepository.findByIdAndIsDeletedFalse(userId)
+            ?: throw BadRequestException(ResponseCode.USER_NOT_FOUND)
+
+        val shop = Shop.create(
+            user = user,
+            name = req.name,
+            addressBasic = req.addressBasic,
+            addressLat = req.addressLat,
+            addressLng = req.addressLng,
+            addressDetail = req.addressDetail,
+        )
+        shopRepository.save(shop)
+
+        return shop.id
+    }
+
+    fun updateShop(userId: String, shopId: String, req: UpdateShopReq) {
+        val user = userRepository.findByIdAndIsDeletedFalse(userId)
+            ?: throw BadRequestException(ResponseCode.USER_NOT_FOUND)
+
+        val shop = shopRepository.findByIdAndUserIdAndIsDeletedFalse(shopId, user.id)
+            ?: throw BadRequestException(ResponseCode.SHOP_NOT_FOUND)
+
+        shop.addressBasic = req.addressBasic
+        shop.addressLat = req.addressLat
+        shop.addressLng = req.addressLng
+        shop.addressDetail = req.addressDetail
+
+        shopRepository.save(shop)
+    }
+
+    fun deleteShop(userId: String, shopId: String) {
+        val user = userRepository.findByIdAndIsDeletedFalse(userId)
+            ?: throw BadRequestException(ResponseCode.USER_NOT_FOUND)
+
+        val shop = shopRepository.findByIdAndUserIdAndIsDeletedFalse(shopId, user.id)
+            ?: throw BadRequestException(ResponseCode.SHOP_NOT_FOUND)
+
+        shop.isDeleted = true
+
+        shopRepository.save(shop)
+    }
 }
