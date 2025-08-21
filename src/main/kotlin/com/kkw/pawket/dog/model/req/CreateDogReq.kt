ccabde708a881f89a7f26b0e696436585b06bf68
kyle <jjyu9312@gmail.com>
2025-08-22 08:20:42 +0900

    pet -> dog 네이밍 변경

--- src/main/kotlin/com/kkw/pawket/dog/model/req/CreateDogReq.kt
+++ src/main/kotlin/com/kkw/pawket/dog/model/req/CreateDogReq.kt
@@ -3,30 +3,30 @@ package com.kkw.pawket.dog.model.req
 import io.swagger.v3.oas.annotations.media.Schema
 
 data class CreateDogReq(
-    @Schema(description = "Registration number of the pet", example = "123456789")
+    @Schema(description = "Registration number of the dog", example = "123456789")
     val registrationNum: String?,
 
-    @Schema(description = "Name of the pet", example = "콩이")
+    @Schema(description = "Name of the dog", example = "콩이")
     val name: String,
 
     @Schema(description = "Type of the dog", example = "포메라니안")
     val type: String,
 
-    @Schema(description = "Age of the pet", example = "3")
+    @Schema(description = "Age of the dog", example = "3")
     val age: Int,
 
-    @Schema(description = "Weight of the pet ", example = "5")
+    @Schema(description = "Weight of the dog ", example = "5")
     val weight: Int,
 
-    @Schema(description = "Sex of the pet (male/female)", example = "male")
+    @Schema(description = "Sex of the dog (male/female)", example = "male")
     val sex: String,
 
-    @Schema(description = "neutered status of the pet", example = "true")
+    @Schema(description = "neutered status of the dog", example = "true")
     val isNeutered: Boolean,
 
     @Schema(
-        description = "pet details including description, food brand, name, and type",
-        example = "{\"petDescription\": \"Friendly and playful\", \"foodBrand\": \"BrandX\", \"foodName\": \"Premium Dog Food\", \"foodType\": \"Dry\"}"
+        description = "dog details including description, food brand, name, and type",
+        example = "{\"dogDescription\": \"Friendly and playful\", \"foodBrand\": \"BrandX\", \"foodName\": \"Premium Dog Food\", \"foodType\": \"Dry\"}"
     )
     val dogDetails: DogDetails? = null,
 ) {
